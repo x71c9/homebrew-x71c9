4cd709af8213b348af94077b6e5b39bf5dabbf80
x71c9 <108585118+x71c9@users.noreply.github.com>
2026-06-29 18:56:52 +0000

    augenblick: 0.2.4

--- Formula/augenblick.rb
+++ Formula/augenblick.rb
@@ -1,13 +1,13 @@
 class Augenblick < Formula
   desc "Fullscreen eye-blink overlay for X11 and Wayland"
   homepage "https://github.com/x71c9/augenblick"
-  version "0.2.3"
-  url "https://github.com/x71c9/augenblick/releases/download/v0.2.3/augenblick-x86_64-apple-darwin.tar.gz"
-  sha256 "60570b3743dccf2fd20af7ec5cabe7f0353ff002ca0d8befd87c35773314a4cc"
+  version "0.2.4"
+  url "https://github.com/x71c9/augenblick/releases/download/v0.2.4/augenblick-x86_64-apple-darwin.tar.gz"
+  sha256 "5aff3816bf8278464d7bce3b223561e19882f43f329fd0272e767667a8ca8a0a"
 
   if Hardware::CPU.arm?
-    url "https://github.com/x71c9/augenblick/releases/download/v0.2.3/augenblick-aarch64-apple-darwin.tar.gz"
-    sha256 "ccce701b1957bd0f3c3a2de356d48396fa47ab778891585a8912438065be7338"
+    url "https://github.com/x71c9/augenblick/releases/download/v0.2.4/augenblick-aarch64-apple-darwin.tar.gz"
+    sha256 "976cad9a468f76eb0034559ade164e62f1ebb289866263e01e9b10146f8091d3"
   end
 
   def install
