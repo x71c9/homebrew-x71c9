7176fc681666ff1ccf3d27776533fd04c63a39fb
x71c9 <108585118+x71c9@users.noreply.github.com>
2026-07-01 10:04:13 +0000

    qrx: 0.4.2

--- Formula/qrx.rb
+++ Formula/qrx.rb
@@ -1,13 +1,13 @@
 class Qrx < Formula
   desc "CLI tool to capture a screen region, decode any QR code found, and copy the result to clipboard."
   homepage "https://github.com/x71c9/qrx"
-  version "0.4.1"
-  url "https://github.com/x71c9/qrx/releases/download/v0.4.1/qrx-x86_64-apple-darwin.tar.gz"
-  sha256 "06ce4f68fc2836e16ac4c4969d766312ecb467aaa086f424fba1e39a0ce5a3e1"
+  version "0.4.2"
+  url "https://github.com/x71c9/qrx/releases/download/v0.4.2/qrx-x86_64-apple-darwin.tar.gz"
+  sha256 "6eda65caffbc68a4339419ccd13fc64e1e865a6ca387adcb01b701968c20943b"
 
   if Hardware::CPU.arm?
-    url "https://github.com/x71c9/qrx/releases/download/v0.4.1/qrx-aarch64-apple-darwin.tar.gz"
-    sha256 "d69709d4b3931055fd3c408b44b5925ec8a0a2c76e4ee3395bacc68adcc0db61"
+    url "https://github.com/x71c9/qrx/releases/download/v0.4.2/qrx-aarch64-apple-darwin.tar.gz"
+    sha256 "cad7e3d255f0b02fd8395ab04d93843805f5044a5bd28acd4dc6b70118c90445"
   end
 
   def install
