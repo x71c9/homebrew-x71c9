d1e4514adb6854f1ab5e20608d0348d81ac980e0
x71c9 <108585118+x71c9@users.noreply.github.com>
2026-07-08 10:13:15 +0000

    dogma: 2.1.5

--- Formula/dogma.rb
+++ Formula/dogma.rb
@@ -1,16 +1,16 @@
 class Dogma < Formula
   desc "CLI to bridge secrets from any vault backend and infra outputs into sops-encrypted files, deployed to your machines — driven by a single dogma.yml"
   homepage "https://github.com/x71c9/dogma"
-  version "2.1.4"
+  version "2.1.5"
 
   on_intel do
-    url "https://github.com/x71c9/dogma/releases/download/v2.1.4/dogma-x86_64-apple-darwin.tar.gz"
-    sha256 "2f91d7fbea5e4743946c143736789782ed094f4af89a79a8d897f8acf446692c"
+    url "https://github.com/x71c9/dogma/releases/download/v2.1.5/dogma-x86_64-apple-darwin.tar.gz"
+    sha256 "43bb82c261a6e086a5f25c9d1c6567b8ae66724fe9fdabcdf87edb3bb0e8839a"
   end
 
   on_arm do
-    url "https://github.com/x71c9/dogma/releases/download/v2.1.4/dogma-aarch64-apple-darwin.tar.gz"
-    sha256 "da37b17923c6d96a40cfa3da26c89ffbdb4750fa1560c0bd6fab7efa89ace205"
+    url "https://github.com/x71c9/dogma/releases/download/v2.1.5/dogma-aarch64-apple-darwin.tar.gz"
+    sha256 "0be34d61feacaea9e6558dcd708c3c457c05ce087feb8bd96e6d8354972f7d7d"
   end
 
   def install
