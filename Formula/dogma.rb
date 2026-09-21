017f16f93af2b7cd843f35402f66ced5147f440a
x71c9 <108585118+x71c9@users.noreply.github.com>
2026-09-21 14:45:41 +0000

    dogma: 3.2.0

--- Formula/dogma.rb
+++ Formula/dogma.rb
@@ -1,13 +1,13 @@
 class Dogma < Formula
   desc "Bridges secrets from vault backends and infrastructure outputs into sops-encrypted files deployed to NixOS machines"
   homepage "https://github.com/x71c9/dogma"
-  version "3.1.0"
-  url "https://github.com/x71c9/dogma/releases/download/v3.1.0/dogma-x86_64-apple-darwin.tar.gz"
-  sha256 "24d707bfee944e7c8716b6e246399c87fa55070766c5edee3c73747cf8b42f8c"
+  version "3.2.0"
+  url "https://github.com/x71c9/dogma/releases/download/v3.2.0/dogma-x86_64-apple-darwin.tar.gz"
+  sha256 "996bfbd149985ba437006da9d0170b707a80066804ee2bf8b8ecb416b6b364cb"
 
   if Hardware::CPU.arm?
-    url "https://github.com/x71c9/dogma/releases/download/v3.1.0/dogma-aarch64-apple-darwin.tar.gz"
-    sha256 "f7e7b79942d19e49e327bcbdf4436bcc1e68f9b49203460f40ed8ce20393a4b0"
+    url "https://github.com/x71c9/dogma/releases/download/v3.2.0/dogma-aarch64-apple-darwin.tar.gz"
+    sha256 "1f0114c0c27ec51e8e2c3d6f4ccd16d1e710eb515d8c878c0b1181895ac59428"
   end
 
   def install
