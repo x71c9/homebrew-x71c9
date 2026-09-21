48db2265c23940522edd3fde0cc945d9261f25d8
x71c9 <108585118+x71c9@users.noreply.github.com>
2026-09-21 08:58:53 +0000

    tempesta: 0.2.0

--- Formula/tempesta.rb
+++ Formula/tempesta.rb
@@ -1,21 +1,25 @@
 class Tempesta < Formula
   desc "The fastest and lightest bookmark manager CLI written in Rust"
   homepage "https://github.com/x71c9/tempesta"
-  version "0.1.40"
-  url "https://github.com/x71c9/tempesta/releases/download/v0.1.40/tempesta-x86_64-apple-darwin.tar.gz"
-  sha256 "352c6b6089fbee7c146a077e70c4e0ee132e001ba9e7cf54b60f51ec6e883a72"
-  
+  version "0.2.0"
+  url "https://github.com/x71c9/tempesta/releases/download/v0.2.0/tempesta-x86_64-apple-darwin.tar.gz"
+  sha256 "41631ad6726e8ede8abea96dcb286d2c1b62bf0b9a60452d295d1babdeb6c590"
+
   if Hardware::CPU.arm?
-    url "https://github.com/x71c9/tempesta/releases/download/v0.1.40/tempesta-aarch64-apple-darwin.tar.gz"
-    sha256 "3450b8f0b19f30dde553d0dc3b0d51aa1fa91ec66b958bce8c98ca6fac9a28f0"
+    url "https://github.com/x71c9/tempesta/releases/download/v0.2.0/tempesta-aarch64-apple-darwin.tar.gz"
+    sha256 "89038eff2b9cf4c1bdd5082e7c1a9971eafee44b2f83fcee244eba1ac7909f37"
   end
 
   def install
     bin.install "tempesta"
+    bin.install_symlink "tempesta" => "t"
+    bin.install_symlink "tempesta" => "tmps"
+    generate_completions_from_executable(bin/"tempesta", "completion")
+    generate_completions_from_executable(bin/"tempesta", "completion", base_name: "t")
+    generate_completions_from_executable(bin/"tempesta", "completion", base_name: "tmps")
   end
 
   test do
     system "#{bin}/tempesta", "--version"
   end
 end
-
